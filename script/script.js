edfc7dae2ea534f920febd46cc95b4b5a43a6294
George <georgiy.ilchuk2009@gmail.com>
2024-10-05 19:34:20 +0300

    1

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousele').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true, // Відображення крапок для навігації
+      arrows: true, // Відображення стрілок
+    });
+  });
